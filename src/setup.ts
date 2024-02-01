e1c03744868be9d893c965f66d4748a091bd0c42
William Reticena <william.wsd43@gmail.com>
2024-01-31 21:28:32 -0300

    chore(comments): resolve comments

--- src/setup.ts
+++ src/setup.ts
@@ -7,7 +7,7 @@ axios.interceptors.request.use(
         const token = localStorage.getItem('token')
 
         if (token) {
-            request.headers.token = `Bearer ${token}`
+            request.headers.token = token
         }
         return request
     },
