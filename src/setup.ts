6cc329b03083da86c36b4b3da88c24e4c0c05a09
William S. Reticena <William.wsd43@gmail.com>
2024-01-31 21:30:14 -0300

    fix(setup): fix token sending in requests (#18)
    Co-authored-by: William Reticena <wsreticena@viptech.com.br>

--- src/setup.ts
+++ src/setup.ts
@@ -2,4 +2,16 @@ import axios from 'axios'
 
 axios.defaults.baseURL = import.meta.env.VITE_API_URL
 
+axios.interceptors.request.use(
+    request => {
+        const token = localStorage.getItem('token')
+
+        if (token) {
+            request.headers.token = token
+        }
+        return request
+    },
+    error => Promise.reject(error)
+)
+
 window.global ||= window // See: https://stackoverflow.com/questions/72114775/vite-global-is-not-defined
