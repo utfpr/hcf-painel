4244b67ef2ece973f05bde7688f6bb6fd237b370
William Reticena <william.wsd43@gmail.com>
2024-02-05 21:26:22 -0300

    fix(detalhes-tombos): fix the image path

--- src/components/GalleryComponent.jsx
+++ src/components/GalleryComponent.jsx
@@ -3,7 +3,24 @@ import { Component } from 'react'
 import ImageGallery from 'react-image-gallery'
 
 export default class GalleryComponent extends Component {
+    handleClick = () => {
+        if (this.props.fotos.length > 0) window.open(this.props.fotos[0].original, '_blank')
+    }
+
+    handleImageError = e => {
+        e.target.src = 'https://t4.ftcdn.net/jpg/01/39/16/63/240_F_139166369_NdTDXc0lM57N66868lC66PpsaMkFSwaf.jpg'
+    }
+
     render() {
-        return <ImageGallery items={this.props.fotos} />
+        return (
+            <ImageGallery
+                items={this.props.fotos}
+                showPlayButton={false}
+                showFullscreenButton={false}
+                showThumbnails={this.props.fotos.length > 1}
+                onClick={this.handleClick}
+                onImageError={this.handleImageError}
+            />
+        )
     }
 }
--- src/config/api.js
+++ src/config/api.js
@@ -1,5 +1,5 @@
 export const baseUrl = import.meta.env.VITE_IMAGE_BASE_URL
 
-export const fotosBaseUrl = `${baseUrl}/fotos`
+export const fotosBaseUrl = `${baseUrl}`
 
 export default {}
