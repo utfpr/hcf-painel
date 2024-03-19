ba04e41572666902563f3d89cc9da735168c9f78
William S. Reticena <William.wsd43@gmail.com>
2024-03-18 22:19:10 -0300

    fix(setup): redirect to login when token is expired (#38)

--- src/setup.ts
+++ src/setup.ts
@@ -1,4 +1,4 @@
-import axios from 'axios'
+import axios, { AxiosError } from 'axios'
 
 axios.defaults.baseURL = import.meta.env.VITE_API_URL
 
@@ -12,6 +12,19 @@ axios.interceptors.request.use(
         return request
     },
     error => Promise.reject(error)
+)
+
+axios.interceptors.response.use(
+    response => response,
+    (error: AxiosError) => {
+        const err: {error: {code: number}} = error.response?.data as {error: {code: number}}
+
+        if (err.error.code === 401) {
+            localStorage.removeItem('token')
+            window.location.href = '/inicio'
+        }
+        return Promise.reject(error)
+    }
 )
 
 window.global ||= window // See: https://stackoverflow.com/questions/72114775/vite-global-is-not-defined
