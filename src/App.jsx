6c93423b66542d5abc36660e042e775dae245a6e
William Reticena <william.wsd43@gmail.com>
2024-03-23 14:41:35 -0300

    feat(coletores): add coletores menu [wip]

--- src/App.jsx
+++ src/App.jsx
@@ -44,6 +44,8 @@ import './assets/css/FormEnterSystem.css'
 import './assets/css/Main.css'
 import './assets/css/Search.css'
 import 'react-image-gallery/styles/css/image-gallery.css'
+import ListaColetoresScreen from './pages/ListaColetoresScreen'
+import NovoColetorScreen from './pages/NovoColetorScreen'
 
 const PrivateRoute = ({ component: Component, authed, ...rest }) => {
     return (
@@ -103,6 +105,9 @@ export default class App extends Component {
                 <PrivateRoute authed={isCurador()} path="/usuarios" component={ListaUsuariosScreen} />
                 <PrivateRoute authed={isCurador()} path="/herbarios/novo" component={NovoHerbarioScreen} />
                 <PrivateRoute authed={isCurador()} path="/herbarios/:herbario_id" component={NovoHerbarioScreen} />
+                <PrivateRoute authed={isCurador()} path="/coletores/novo" component={NovoColetorScreen} />
+                <PrivateRoute authed={isCurador()} path="/coletores/:coletor_id" component={NovoColetorScreen} />
+                <PrivateRoute authed={isCurador()} path="/coletores" component={ListaColetoresScreen} />
 
                 <Route path="/herbarios" component={ListaHerbariosScreen} />
                 <Route path="/fichas/tombos" component={FichaTomboScreen} />
