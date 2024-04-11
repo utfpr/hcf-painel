a4cb41e204b4f1ea2949830df7a3b43dc2cbe3af
William S. Reticena <William.wsd43@gmail.com>
2024-04-11 17:22:05 -0300

    Optimize the tombo image carousel (#59)

--- src/helpers/fotos-tombo-map.js
+++ src/helpers/fotos-tombo-map.js
@@ -1,6 +1,11 @@
 import { fotosBaseUrl } from '../config/api'
 
 export default foto => ({
-    original: `${fotosBaseUrl}/${foto.original}`,
-    thumbnail: `${fotosBaseUrl}/${foto.thumbnail}`
+    fullscreen: `${fotosBaseUrl}/${foto.original}`,
+    original: `${fotosBaseUrl}/crop/500/400/${foto.original}`,
+    thumbnail: `${fotosBaseUrl}/200/${foto.thumbnail}`,
+    originalAlt: foto.original,
+    originalTitle: foto.original,
+    thumbnailAlt: foto.thumbnail,
+    thumbnailTitle: foto.thumbnail
 })
