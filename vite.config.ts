5909942a7b6d5efecffed9f466f4987a69f9f5e4
Edvaldo Szymonek <edvaldoszy@gmail.com>
2025-10-13 22:30:13 -0300

    adiciona possibilidade de definir a base da url

--- vite.config.ts
+++ vite.config.ts
@@ -4,23 +4,26 @@ import { defineConfig } from 'vite'
 
 import react from '@vitejs/plugin-react'
 
+const baseUrl = process.env.BASE_URL ?? '/'
+
 export default defineConfig({
-    plugins: [
-        react(),
-        {
-            name: 'html-transform',
-            transformIndexHtml(html) {
-                return html.replace(
-                    /%VITE_RECAPTCHA_SITE_KEY%/g,
-                    process.env.VITE_RECAPTCHA_SITE_KEY || ''
-                )
-            }
-        }
-    ],
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, './src'),
-            '#': path.resolve(__dirname, './test')
-        }
+  base: baseUrl,
+  plugins: [
+    react(),
+    {
+      name: 'html-transform',
+      transformIndexHtml(html) {
+        return html.replace(
+          /%VITE_RECAPTCHA_SITE_KEY%/g,
+          process.env.VITE_RECAPTCHA_SITE_KEY ?? ''
+        )
+      }
     }
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '#': path.resolve(__dirname, './test')
+    }
+  }
 })
