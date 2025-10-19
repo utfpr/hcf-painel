6f31b0ee32dbb98f8cd1ac595aefe3a8b16b90b7
Edvaldo Szymonek <edvaldoszy@gmail.com>
2025-10-18 22:44:53 -0300

    usa baseUrl com prefixo VITE_

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite'
 
 import react from '@vitejs/plugin-react'
 
-const baseUrl = process.env.BASE_URL || '/'
+const baseUrl = process.env.VITE_BASE_URL || '/'
 
 console.log('baseUrl', baseUrl)
 
