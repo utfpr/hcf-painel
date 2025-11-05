581e475b74e5ad3f3b383f37c3bf4f1bb995fd96
Matheus Coitinho <142349293+MatCoitinho@users.noreply.github.com>
2025-11-04 22:38:48 -0300

    Remoção de Código do Telefone em Estado (#239)

--- src/components/estados/Estados.container.jsx
+++ src/components/estados/Estados.container.jsx
@@ -153,7 +153,6 @@ const ListaEstadosContainer = () => {
             form.setFieldsValue({
                 nomeEstado: estado.nome,
                 ufEstado: estado.sigla,
-                codigoTelefone: estado.codigo_telefone,
                 paisId: estado.pais_id,
             });
         } else {
