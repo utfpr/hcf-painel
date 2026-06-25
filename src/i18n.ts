0f8dad79e172d4c138bf1d705a31b5c2c8186599
Lucas Vaz <lucasvazlira14@gmail.com>
2026-06-24 23:27:36 -0300

    Internacionalizacao da tela de coletores

--- src/i18n.ts
+++ src/i18n.ts
@@ -55,7 +55,9 @@ void i18n
       'relatorioFamiliasGenero',
       'authService',
       'listaTaxonomiaVariedade',
-      'exportacaoScreen'
+      'exportacaoScreen',
+      'listaColetoresScreen',
+      'novoColetorScreen'
     ],
     defaultNS: 'common',
 
