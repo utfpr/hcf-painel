68cf077700b81a4232c0983474dd150cd081dfec
Lucas Vaz <lucasvazlira14@gmail.com>
2026-06-25 12:24:52 -0300

    Internacionalizacao da tela de usuários

--- src/i18n.ts
+++ src/i18n.ts
@@ -55,7 +55,9 @@ void i18n
       'relatorioFamiliasGenero',
       'authService',
       'listaTaxonomiaVariedade',
-      'exportacaoScreen'
+      'exportacaoScreen',
+      'listaUsuariosScreen',
+      'novoUsuarioScreen'
     ],
     defaultNS: 'common',
 
