c29b1fb81b3814f53bc56d662002331973dad682
Lucas Dos Santos Vaz <52181258+luscas18@users.noreply.github.com>
2026-07-01 18:38:24 -0300

    Internacionalizacao da tela de herbários (#358)
    Co-authored-by: Edvaldo Szymonek <edvaldoszy@gmail.com>

--- src/i18n.ts
+++ src/i18n.ts
@@ -56,6 +56,8 @@ void i18n
       'authService',
       'listaTaxonomiaVariedade',
       'exportacaoScreen',
+      'listaHerbariosScreen',
+      'novoHerbarioScreen',
       'listaRemessasScreen',
       'novaRemessaScreen',
       'listaUsuariosScreen',
