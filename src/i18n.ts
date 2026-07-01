055ed6d74049c20313b33a702b64f2462341d46f
Lucas Dos Santos Vaz <52181258+luscas18@users.noreply.github.com>
2026-07-01 07:52:19 -0300

    Internacionalizacao da tela de relatório de coordenada fora do poligno (#361)
    Co-authored-by: Edvaldo Szymonek <edvaldoszy@gmail.com>

--- src/i18n.ts
+++ src/i18n.ts
@@ -56,6 +56,7 @@ void i18n
       'authService',
       'listaTaxonomiaVariedade',
       'exportacaoScreen',
+      'relatorioCoordenadaForaPoligonoScreen',
       'relatorioLocalColetaPeriodoScreen',
       'relatorioQtdPeriodoScreen',
       'relatorioTombosPorCidadeScreen'
