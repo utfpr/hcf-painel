abcdb405dae5f869059114970d8ada1398f89ca3
Josue Modesto <91280773+JosueModesto@users.noreply.github.com>
2026-07-07 08:23:39 -0300

    Internacionalização da tela ServicosSpeciesLinkScreen (#352)
    Co-authored-by: Edvaldo Szymonek <edvaldoszy@gmail.com>

--- src/i18n.ts
+++ src/i18n.ts
@@ -56,6 +56,7 @@ void i18n
       'authService',
       'listaTaxonomiaVariedade',
       'exportacaoScreen',
+      'servicosSpeciesLink',
       'servicosReflora',
       'verPendencia',
       'livroTombo',
