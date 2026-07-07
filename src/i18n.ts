09b1ed2c6e710f4c6ec33f7d6cfc77027620daa0
Lucas Dos Santos Vaz <52181258+luscas18@users.noreply.github.com>
2026-07-07 08:09:39 -0300

    Internacionalizacao da tela de coletores (#357)
    Co-authored-by: Edvaldo Szymonek <edvaldoszy@gmail.com>

--- src/i18n.ts
+++ src/i18n.ts
@@ -56,6 +56,8 @@ void i18n
       'authService',
       'listaTaxonomiaVariedade',
       'exportacaoScreen',
+      'listaColetoresScreen',
+      'novoColetorScreen',
       'listaHerbariosScreen',
       'novoHerbarioScreen',
       'listaRemessasScreen',
